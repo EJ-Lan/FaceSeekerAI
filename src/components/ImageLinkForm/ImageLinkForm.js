a81c9f7b69fa760b1906024d17c4f7ec65d607c3
EJ-Lan <ervinjon_lanada@outlook.com>
2023-08-28 13:31:24 -0600

    Add Logo, Navigation, and Other Components

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+    return (
+        <div >
+            <p className='f3'>
+                {'This Face Seeker AI will detect faces in your pictures. Give it a Try!'}
+            </p>
+            <div className='center'>
+                <div className='pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='tex' />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-pink'>Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
