f8e20cbad608a86a0dd5d7fff54de06b0b450d72
EJ-Lan <ervinjon_lanada@outlook.com>
2023-09-02 01:03:34 -0600

    Add some event handlers

--- src/App.js
+++ src/App.js
@@ -6,16 +6,37 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg color="#ffc0cb" num={200} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg color="#ffc0cb" num={200} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+
+        />
+      </div>
+    );
+  } 
 }
 
 export default App;
